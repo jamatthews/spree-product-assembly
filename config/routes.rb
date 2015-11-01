be5305519aa87012066a0550557fd09331c5d674
Jacob Matthews <jake@hell.co.nz>
2015-11-01 23:16:01 +1300

    re-enable routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Spree::Core::Engine.add_routes do
   
-  if defined? Spree::Admin::ProductsController
-    namespace :admin do
-      resources :products do
-        resources :parts do
-          member do
-            post :select
-            post :remove
-            post :set_count
-          end
-          collection do
-            post :available
-            get  :selected
-          end
+
+  namespace :admin do
+    resources :products do
+      resources :parts do
+        member do
+          post :select
+          post :remove
+          post :set_count
+        end
+        collection do
+          post :available
+          get  :selected
         end
       end
     end
