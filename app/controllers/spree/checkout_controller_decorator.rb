98d35eb165938ed2058d4d4a51bf0d777b4b7589
Jacob Matthews <jake@hell.co.nz>
2015-11-09 12:30:46 +1300

    dont break sprees saved payment methods

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -3,7 +3,11 @@ module Spree
     CheckoutController.class_eval do
       # Override because we don't want to remove unshippable items from the order
       # A bundle itself is an unshippable item
-      def before_payment; end
+      def before_payment
+        if try_spree_current_user && try_spree_current_user.respond_to?(:payment_sources)
+          @payment_sources = try_spree_current_user.payment_sources
+        end
+      end
     end
   end
 end
